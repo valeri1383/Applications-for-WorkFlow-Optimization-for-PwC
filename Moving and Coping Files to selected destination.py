00e43bf5545eecc96682674229381822a09e19cb
valeri1383 <valeri1383@gmail.com>
2023-06-09 11:14:36 +0100

    Add files via upload

--- Moving and Coping Files to selected destination.py
+++ Moving and Coping Files to selected destination.py
@@ -1,4 +1,5 @@
 import csv
+import time
 from datetime import datetime
 import os
 import shutil
@@ -17,7 +18,7 @@ if not os.path.exists(destination_folder):
 def move_files_by_extension_and_folders_by_name():
     # Get a list of all files in the source directory
     files = os.listdir(source_folder)
-    file_extension = input("What type of files (extensions) would you like to move: ")
+    file_extension = input("What type of Files (extensions) or Folders would you like to move: ")
 
     # Filter files by extension
     filtered_files = [file for file in files if file.endswith(file_extension)]
@@ -27,7 +28,8 @@ def move_files_by_extension_and_folders_by_name():
         source_path = os.path.join(source_folder, file)
         destination_path = os.path.join(destination_folder, file)
         shutil.move(source_path, destination_path)
-    print('All done.')
+    time.sleep(1.5)
+    print('\nAll done.')
 
 
 # move all files and folders
@@ -36,20 +38,23 @@ def move_all_files_and_folders():
         # construct full file path
         source = source_folder + file_name
         destination = destination_folder + file_name
-        # move only files
+        # move files and folders
         shutil.move(source, destination)
-    print('All done.')
+    time.sleep(1.5)
+    print('\nAll done.')
 
 
 def list_all_files():
     files = os.listdir(source_folder)
-    print("All Available files:")
+    print(" All Available files: ")
     for x in files:
+        time.sleep(0.3)
         print(f"    - {x}")
     print()
+    time.sleep(2)
 
 
-def copy_all_files_from_desktop():  # Copy only file without folders due to the permission
+def copy_all_files_from_desktop():
     for file_name in os.listdir(source_folder):
         # construct full file path
         file_path = os.path.join(os.environ["USERPROFILE"], f"Desktop\\{file_name}")
@@ -62,14 +67,15 @@ def copy_all_files_from_desktop():  # Copy only file without folders due to the 
             destination = destination_folder + file_name
             # copy only files
             shutil.copy(source, destination)
-    print('All done.')
+    time.sleep(1.5)
+    print('\nAll done.')
 
 
 
 def copy_file_by_extention_and_folders_by_name():
     # Get a list of all files in the source directory
     files = os.listdir(source_folder)
-    file_extension = input("What type of files (extensions) would you like to copy: ")
+    file_extension = input("What type of files (extensions) or Folders would you like to copy: ")
 
     # Filter files by extension
     filtered_files = [file for file in files if file.endswith(file_extension)]
@@ -84,8 +90,9 @@ def copy_file_by_extention_and_folders_by_name():
         else:
             source_path = os.path.join(source_folder, file)
             destination_path = os.path.join(destination_folder, file)
-            shutil.copy(source_path, destination_path)  # Can't copy folder due to permissions
-    print('All done.')
+            shutil.copy(source_path, destination_path)
+    time.sleep(1.5)
+    print('\nAll done!')
 
 
 # getting the GUID username
@@ -126,23 +133,31 @@ with open('Users Moved Data.csv', 'a', newline='') as csvfile:
 
 
 def menu():
+    os.system('color B0')
     user_choice = 1
     while True:
 
         if user_choice == 5:
             break
 
-        print("MOVING and COPING App")
+        print()
+        print('*' * 4 +' MOVING and COPING App ' + '*' * 4 + '\n')
+        time.sleep(2)
         print("Powered by Valeri Vasilev\n")
+        time.sleep(2)
         options = ["    1. Move Files by extension or Folders by name\n"  # done
-                   "    2. Move all Filed and Folders\n"  # to be tested extra
+                   "    2. Move all Files and Folders\n"  # to be tested extra
                    "    3. Copy files by extension and Folders by name\n"  # done
                    "    4. Copy all Files and Folders\n"      # done
                    "    5. Exit\n"]
+
         for x in options:
             print(x)
 
-        user_choice = int(input("What would you like to do? "))
+
+        time.sleep(2)
+        user_choice = int(input("-- What would you like to do? "))
+        print()
 
         if user_choice in [1, 2, 3, 4]:
             list_all_files()
@@ -161,7 +176,10 @@ def menu():
         else:
             print("Wrong choice option selected!!! Please try again.\n")
 
-
+        user_select = input("\nWould you like to continue? Y/N: ").lower()
+        if user_select == 'n':
+            print("\nExiting... Have a nice day!")
+            break
 
 
 menu()
