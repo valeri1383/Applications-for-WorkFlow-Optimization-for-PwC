0bc9ba4b72d168d818a72fd1f29d0af9148f9d78
valeri1383 <valeri1383@gmail.com>
2023-06-16 16:59:53 +0100

    Add files via upload

--- Listing elements with Tkinter.py
+++ Listing elements with Tkinter.py
@@ -1,28 +1,22 @@
+import os
 import tkinter as tk
 import time
 
 window = tk.Tk()
-window.geometry("500x300")
+window.geometry("300x700")
 
 # Create a list of element texts
-element_texts = ["Element 1", "Element 2", "Element 3", "Element 4", "Element 5"]
+source_folder = os.path.join(os.environ["USERPROFILE"], "Desktop\\")
+element_texts = os.listdir(source_folder)
 
-# Function to create and pack elements with delay
+
 def create_elements_with_delay():
     for i, text in enumerate(element_texts):
-        # Create the element
         element = tk.Label(window, text=text)
-
-        # Pack the element
         element.pack()
-
-        # Update the window to show the new element
         window.update()
-
-        # Delay for 1 second
         time.sleep(0.5)
 
-# Call the function to create and pack elements with delay
 create_elements_with_delay()
 
 window.mainloop()
