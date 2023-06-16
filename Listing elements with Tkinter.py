737bac945ac75159b60677a2acad773f39887842
valeri1383 <valeri1383@gmail.com>
2023-06-16 17:25:46 +0100

    Add files via upload

--- Listing elements with Tkinter.py
+++ Listing elements with Tkinter.py
@@ -4,6 +4,8 @@ import time
 
 window = tk.Tk()
 window.geometry("300x700")
+window.configure(bg='misty rose',border=5)
+window.title('Files on the Desktop')
 
 # Create a list of element texts
 source_folder = os.path.join(os.environ["USERPROFILE"], "Desktop\\")
@@ -12,11 +14,14 @@ element_texts = os.listdir(source_folder)
 
 def create_elements_with_delay():
     for i, text in enumerate(element_texts):
-        element = tk.Label(window, text=text)
+        element = tk.Label(window,fg='navy', bg='misty rose', text=text)
         element.pack()
         window.update()
-        time.sleep(0.5)
+        time.sleep(0.3)
+
+    window.destroy()
 
 create_elements_with_delay()
+
 
 window.mainloop()
